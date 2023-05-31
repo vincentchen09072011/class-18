c14620535c9ce1affb47872ab7832ce1b25a6944
vincentchen09072011 <122748304+vincentchen09072011@users.noreply.github.com>
2023-05-30 21:19:54 -0400

    Add files via upload

---
+++ inde.ts
@@ -0,0 +1,14 @@
+import axios from 'axios'
+const ApiKey = '46c5ab3a7ed8c71a8b32bb28e9ff8643'
+const movieurl = `https://api.themoviedb.org/3/trending/tv/day?api_key=${ApiKey}`
+
+const movies = axios.get(movieurl).then((response) => {
+    const titles = response.data.results.map((tv:any) => tv.name)
+    const titleandoverview = response.data.results.map((tv:any) => ({
+        title:tv.name,
+        overview:tv.overview
+    }))
+    const moviefilter = response.filter(word => word.length > 10).map(word => word.toUpperCase());
+    console.log(titleandoverview)
+    console.log(titles)
+})
